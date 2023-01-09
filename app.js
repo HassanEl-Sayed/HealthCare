2da76c973f762c5d1d612149d28765cbaaf5572a
Hassan ElSayed <hs247319@example.com>
2023-01-10 00:34:12 +0200

    HealthCare

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 const cors = require('cors')
 var logger = require('morgan');
 require('./dbConnection/mongoose')
